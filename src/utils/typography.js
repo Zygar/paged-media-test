dceea22a990931cc915a3482375b3009fca52271
Andy Wilkinson <a@andywilkinson.co.nz>
2021-08-01 19:36:25 +1200

    theme updates

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,32 +1,44 @@
 import Typography from "typography"
-// import theme from 'typography-theme-lincoln'
+import theme from 'typography-theme-lincoln'
 
 const typography = new Typography(
     {
-        baseFontSize: "18px",
-        baseLineHeight: 1.51,
-        headerFontFamily: [
-            "Avenir Next",
-            "Helvetica Neue",
-            "Segoe UI",
-            "Helvetica",
-            "Arial",
-            "sans-serif",
-        ],
-        bodyFontFamily: ["Georgia", "serif"],
-        overrideStyles: ({ rhythm, scale }, options) => {
-            return {
-                h1: {
-                    fontFamily: ['Arial', 'sans-serif'].join(','),
-                },
-                "@media only print": {
-                    html: {
-                        fontSize: "9pt"
-                    }
+        ...theme,
+        overrideThemeStyles: () => ({
+            "@media only print": {
+                html: {
+                    fontSize: "9pt"
                 }
             }
-        }
-    }
-)
+        })
+    });
+
+// const typography = new Typography(
+//     {
+//         baseFontSize: "18px",
+//         baseLineHeight: 1.51,
+//         headerFontFamily: [
+//             "Avenir Next",
+//             "Helvetica Neue",
+//             "Segoe UI",
+//             "Helvetica",
+//             "Arial",
+//             "sans-serif",
+//         ],
+//         bodyFontFamily: ["Georgia", "serif"],
+//         overrideStyles: ({ rhythm, scale }, options) => {
+//             return {
+//                 h1: {
+//                     fontFamily: ['Arial', 'sans-serif'].join(','),
+//                 },
+// "@media only print": {
+//     html: {
+//         fontSize: "9pt"
+//     }
+// }
+//             }
+//         }
+//     }
+// )
 
 export default typography
