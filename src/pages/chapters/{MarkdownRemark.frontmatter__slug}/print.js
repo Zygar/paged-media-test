f10649b5b1b42d1acf51fcf2b6f928bc18152fbc
Andy Wilkinson <a@andywilkinson.co.nz>
2021-08-01 18:12:47 +1200

    Horrendous code, but print URL now matches print output

--- src/pages/chapters/{MarkdownRemark.frontmatter__slug}/print.js
+++ src/pages/chapters/{MarkdownRemark.frontmatter__slug}/print.js
@@ -10,16 +10,38 @@ export default function Template({
 }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
+  React.useEffect(() => {
+    const fragmentPrep = function (html) {
+      const parser = new DOMParser();
+      let wrappedHtml = `<section class='post'>${html}</section>`
+      let parsed = parser.parseFromString(wrappedHtml, "text/html");
+      let parsedFilteredDom = parsed.querySelectorAll('body > *');
+      console.log(parsedFilteredDom);
+      let fragment = document.createDocumentFragment();
+      for (let i = 0; i < parsedFilteredDom.length; i++) {
+        fragment.appendChild(parsedFilteredDom[i]);
+      }
+      return fragment
+    }
+    let fragment = fragmentPrep(html);
+    console.log(fragment);
+
+    const previewer = new Previewer();
+    let target = document.querySelector('.page-wrapper')
+    previewer.preview(fragment, false, target)
+    console.log(document.body)
+  });
   return (
     <div className="page-wrapper">
-      <div className="blog-post">
+      {/* <div className="blog-post">
         <Link to="/">Go home</Link>
         <h1>{frontmatter.title}</h1>
         <p>{frontmatter.author}</p>
-        <Chapter html={html}></Chapter>
+         */}
+      {/* <Chapter html={html}></Chapter> */}
 
-      </div>
     </div>
+    // </div>
   )
 }
 
