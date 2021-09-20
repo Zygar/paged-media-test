35e7eed735152ee4dac1d3f454c8d054a5fdcfe2
Andy Wilkinson <a@andywilkinson.co.nz>
2021-09-20 14:00:19 +1200

    injects styled-components styles inline when headless

---
+++ gatsby-browser.js
@@ -0,0 +1,17 @@
+exports.onClientEntry = () => {
+    const styledComponents = document.querySelector('style[data-styled]');
+    var isAutomated = navigator.webdriver;
+
+    if (styledComponents != null && isAutomated == true) {
+        var inlineStyles = testVariable.innerHTML;
+        var head = document.head || document.getElementsByTagName('head')[0];
+        var style = document.createElement('style');
+        head.appendChild(style);
+        style.type = 'text/css';
+        style.innerHTML = inlineStyles;
+
+    } else {
+
+    }
+
+}
